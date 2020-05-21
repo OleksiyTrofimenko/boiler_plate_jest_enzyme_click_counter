2237903cc3f3a1c2328eb0b575aefb7efdfd0e45
oleksiy <bakke87@gmail.com>
2020-05-21 09:28:54 +0300

    Starter

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdaptor from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdaptor() });
+
+test('renders without errors', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
 });
+test('renders increment counter', () => {});
+test('render counter display', () => {});
+test('counter start at 0', () => {});
+test('clicking button increment counter display', () => {});
