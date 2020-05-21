cf08e8b99858b1b13030f1115006dd32c03d8184
oleksiy <bakke87@gmail.com>
2020-05-21 11:32:16 +0300

    TDD - Click counter component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React, { PureComponent } from 'react';
 
 class App extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    };
+  }
+
   render() {
-    return <div data-test='component-app'>App</div>;
+    return (
+      <div data-test='component-app'>
+        <h1 data-test='counter-display'>Counter {this.state.counter}</h1>
+        <button
+          onClick={() =>
+            this.setState({
+              counter: this.state.counter + 1,
+            })
+          }
+          data-test='increment-button'>
+          Increment
+        </button>
+      </div>
+    );
   }
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,81 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, ShallowWrapper } from 'enzyme';
 import EnzymeAdaptor from 'enzyme-adapter-react-16';
 import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdaptor() });
 
+/**
+ * Factory function to create a ShallowWrapper for the App component
+ * @function setup
+ * @param {object} props - Component props specific to this setup
+ * @param {object} state - Initial state for setup
+ * @returns {ShallowWrapper}
+ */
+
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App />);
+
+  if (state) {
+    wrapper.setState(state);
+  }
+
+  return wrapper;
+};
+
+/**
+ * Return ShallowWrapper contained node(s) with given data-test value
+ * @param {ShallowWrapper} wrapper
+ * @param {string} val - Value of data-test attribute for search.
+ * @returns {ShallowWrapper}
+ */
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test='${val}']`);
+};
+
 test('renders without errors', () => {
   const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
+
+  // Find element: Solution without helper
+  // const appComponent = wrapper.find("[data-test='component-app']");
+
+  // Find element: Solution with helper
+  const appComponent = findByTestAttr(wrapper, 'component-app');
+
   expect(appComponent.length).toBe(1);
 });
-test('renders increment counter', () => {});
-test('render counter display', () => {});
-test('counter start at 0', () => {});
-test('clicking button increment counter display', () => {});
+
+test('renders increment counter', () => {
+  const wrapper = shallow(<App />);
+  const button = findByTestAttr(wrapper, 'increment-button');
+
+  expect(button.length).toBe(1);
+});
+
+test('render counter display', () => {
+  const wrapper = shallow(<App />);
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+
+  expect(counterDisplay.length).toBe(1);
+});
+
+test('counter start at 0', () => {
+  const wrapper = setup();
+  const initialCounter = wrapper.state('counter');
+
+  expect(initialCounter).toBe(0);
+});
+
+test('clicking button increment counter display', () => {
+  const counter = 7;
+  const wrapper = setup(null, { counter });
+
+  // Find button and click
+  const button = findByTestAttr(wrapper, 'increment-button');
+  button.simulate('click');
+
+  // Find display and test value
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.text()).toContain(counter + 1);
+});
